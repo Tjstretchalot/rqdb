3b32140495026a0ff06f7b1809d99000c7e4ae0d
Timothy Moore <timothy@geniosai.ai>
2026-06-24 10:33:01 -0700

    Fix infinite retries

--- src/rqdb/async_connection.py
+++ src/rqdb/async_connection.py
@@ -296,12 +296,14 @@ class AsyncConnection:
         index = 0
         attempt = 1
 
-        while index < len(self.hosts) or attempt < self.max_attempts_per_host:
+        while True:
             if index + 1 < len(self.hosts):
                 index += 1
             else:
                 index = 0
                 attempt += 1
+                if attempt > self.max_attempts_per_host:
+                    break
 
             if resp := await attempt_host(node_ordering[index], node_path):
                 return resp
